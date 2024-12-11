6534a549597b038701e4e533b6dc5d5b4e23eb57
AlexisEgea <alexisegea@outlook.com>
2024-12-11 13:11:50 +0100

    Adding launcher script to facilitate the project execution

--- src/launcher_policy.py
+++ src/launcher_policy.py
@@ -1,3 +1,4 @@
+import os
 import random
 from src.player.humane import Humane
 from src.game.game_421 import Game_421
@@ -8,7 +9,14 @@ if __name__ == '__main__':
     game = Game_421()
 
     humane = Humane()
-    random_bot = RandomBot(game)
+
+    data_path = os.path.join(os.getcwd(), "src/data/421.csv")
+    if not os.path.exists(data_path):
+        random_bot = RandomBot(game)
+        game.start_game(random_bot, 10000)
+        score_random = game.score
+
+    game = Game_421()
     policy_bot = PolicyBot(game)
     game.start_game(policy_bot, 10000)
 
