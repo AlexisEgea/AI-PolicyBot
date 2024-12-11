b733750b8f5c101ca6425f8b54f67a28b371ac1c
AlexisEgea <alexisegea@outlook.com>
2024-12-11 13:13:51 +0100

    Adding launcher for policy bot player

--- src/launcher_policy.py
+++ src/launcher_policy.py
@@ -1,22 +1,19 @@
 import os
-import random
-from src.player.humane import Humane
-from src.game.game_421 import Game_421
-from src.player.random_bot import RandomBot
-from src.player.policy_bot import PolicyBot
+from game.game_421 import Game_421
+from player.random_bot import RandomBot
+from player.policy_bot import PolicyBot
 
 if __name__ == '__main__':
     game = Game_421()
 
-    humane = Humane()
-
-    data_path = os.path.join(os.getcwd(), "src/data/421.csv")
+    # To use the policy bot, we need a dataset to create a policy. If this dataset does not exist,
+    # we need to execute the random bot on 10,000 games to generate it.
+    data_path = os.path.join(os.getcwd(), "data/421.csv")
     if not os.path.exists(data_path):
         random_bot = RandomBot(game)
         game.start_game(random_bot, 10000)
-        score_random = game.score
+        game = Game_421()
 
-    game = Game_421()
     policy_bot = PolicyBot(game)
     game.start_game(policy_bot, 10000)
 
