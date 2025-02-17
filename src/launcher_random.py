c96a5573f467ef06195454d3c375d878e2737c06
AlexisEgea <alexisegea@outlook.com>
2025-02-17 19:37:34 +0100

    Adding launcher

--- src/launcher_random.py
+++ src/launcher_random.py
@@ -1,12 +1,9 @@
 from game.game_421 import Game_421
+from game.launcher import Launcher
 from player.random_bot import RandomBot
 
 if __name__ == '__main__':
     game = Game_421()
     random_bot = RandomBot(game)
-    game.start_game(random_bot, 10000)
-
-
-
-
-
+    launcher = Launcher(game, random_bot)
+    launcher.start_game(10000)
